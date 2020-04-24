8dd156050d0ade62b988fa3c10b1edd530480a15
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 16:59:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,6 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  Reys_favorite_things = {"food" => "salmon and pea dry food" "toy" => "wire chaser" "person" => "me" "place to sleep" => "bean bag"}
+end
 
   
 end
