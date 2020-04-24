73202ab6f720e5645a9629eacfc86d8205de6ca7
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 17:00:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  Reys_favorite_things = {"food" => "salmon and pea dry food" "toy" => "wire chaser" "person" => "me" "place to sleep" => "bean bag"}
+  Reys_favorite_things = {"food" => "salmon and pea dry food", "toy" => "wire chaser", "person" => "me", "place to sleep" => "bean bag"}
 end
 
 
